aa1becf4861b2acd30b76689d8382db63936397b
Josse Van Delm <josse.vandelm@gmail.com>
2021-02-19 20:09:10 +0100

    Add Relay tutorial and refactor Relay code

--- sirius/relay/ews_mat.py
+++ sirius/relay/ews_mat.py
@@ -1,6 +1,6 @@
-#******************************************************************
-# In this Relay example I try to implement a simple matrix addition
-#******************************************************************
+#*************************************************************************
+# (LLVM) In this Relay example I try to implement a simple matrix addition
+#*************************************************************************
 
 import tvm
 import tvm.relay as relay
@@ -25,9 +25,8 @@ sum_expr = relay.add(a,b)
 module = tvm.ir.IRModule()
 module = module.from_expr(sum_expr)
 
-# Define a target for compilation and a runtime context for cpu
+# Define a target for compilation
 target = tvm.target.Target("llvm")
-ctx = tvm.cpu()
 
 # Optimize (?)  and build the relay code:
 with tvm.transform.PassContext(opt_level=3):
@@ -36,14 +35,15 @@ with tvm.transform.PassContext(opt_level=3):
 # At this point you can export the library as a shared library file (.so)
 #lib.export_library("compiled_lib.so")
 
-# Put the generated library to work in a runtime module
+# Put the generated library to work in a runtime module with cpu context
+ctx = tvm.cpu()
 runtime_module = graph_runtime.GraphModule(lib["default"](ctx))
 
 # create two random tensors...
 a = np.random.randint(0,5,size=tensor_shape).astype(data_type)
 b = np.random.randint(0,5,size=tensor_shape).astype(data_type)
 
-# ... and set them as input for the module 
+# ... and set them as input for the module
 runtime_module.set_input("a", a)
 runtime_module.set_input("b", b)
 
@@ -53,7 +53,7 @@ runtime_module.run()
 # Get the outputs and copy them to an empty numpy array
 out = runtime_module.get_output(0, tvm.nd.empty(tensor_shape,dtype=data_type)).asnumpy()
 
-# Print the in and outputs
+# Print the in and outputs and compare to see if TVM made something that makes sense
 print("a:")
 print(a)
 print("b:")
