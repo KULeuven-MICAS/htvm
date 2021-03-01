bea24acfe7da01ccb33717de6a4a74372b623f6c
roadelou <tableau.roche@gmail.com>
2021-03-01 17:26:16 +0100

    Added script to test resnet18 on SIRIUS

---
+++ sirius/relay/sirius_resnet18.py
@@ -0,0 +1,19 @@
+from tvm import relay
+from tvm.relay import testing
+from tvm.contrib import utils
+import tvm
+
+resnet18_mod, resnet18_params = relay.testing.resnet.get_workload(num_layers=18)
+
+# build
+with tvm.transform.PassContext(opt_level=3, config={'tir.disable_vectorize':True}):
+    resnet18_lib = relay.build(resnet18_mod, "sirius", params=resnet18_params)
+
+# path lib
+file_name = "resnet.so"
+
+# Getting module
+module = resnet18_lib.get_lib()
+
+# export library
+module.export_library(file_name, workspace_dir="/tmp/")
