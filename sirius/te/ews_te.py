a80fc080a9f13ac8a16f11f149608f6851bc1019
Josse Van Delm <josse.vandelm@gmail.com>
2021-03-12 18:52:27 +0100

    Remove wrong comments/print statements and improve readability

--- sirius/te/ews_te.py
+++ sirius/te/ews_te.py
@@ -1,4 +1,4 @@
-# Calculate Simple 4x4 element wise sum and tensorize over  2x2 matrices
+# Calculate Simple (arbitrary dimensional) element wise sum and tensorize over 2x2 matrices
 # Also look into:
 #
 # *  https://tvm.apache.org/docs/tutorials/language/tensorize.html
@@ -22,9 +22,8 @@ def intrin_ews(ro,co,data_type,stride):
     print(tvm.lower(preview, [a, b, c], simple_mode=True))
 
     # Define buffers
-    # Offset factor --> optimize for vectorized buffering
-    # Strides are set by the factors that appear near the i.inner and j.inner
-    # In this case i.inner corresponds to the columnn dimension of the tensor, so:
+    # Offset factor --> so TVM can optimize for vectorized buffering
+    # Stride        --> see TVM discuss post above
     Ab = tvm.tir.decl_buffer(a.shape, a.dtype, name="A", offset_factor=1, strides=[stride,1])
     Bb = tvm.tir.decl_buffer(b.shape, b.dtype, name="B", offset_factor=1, strides=[stride,1])
     Cb = tvm.tir.decl_buffer(c.shape, c.dtype, name="C", offset_factor=1, strides=[stride,1])
@@ -51,38 +50,40 @@ def intrin_ews(ro,co,data_type,stride):
 
     return te.decl_tensor_intrin(c.op, intrin_func, binds={a: Ab, b: Bb, c: Cb})
 
-# Dimensions of tensorization
+# Dimensions of tensorization intrinsic
 rows = 2
 cols = 2
-data_type = "float32"
-# Create an instance
-
+data_type = "int8"
 
 # Dimensions of tensor to be tensorized
 ro = 26
 co = 26
 dim1 = 6
 dim2 = 2
+
 # Create a tensorizable schedule
 A = te.placeholder((ro,co,dim1,dim2), dtype=data_type, name="A")
 B = te.placeholder((ro,co,dim1,dim2), dtype=data_type, name="B")
 # C = te.compute((ro,co,dim1,dim2), lambda i,j,k,l: A[i,j,k,l] + B[i,j,k,l], name="T_add")
 # Using topi.add implementation here to reflect schedule of relay graph
 C = topi.add(A,B)
+
 # Create a vanilla schedule
 s = te.create_schedule(C.op)
 print("Larger schedule to apply tensorization of the Generic Schedule (before tiling):")
 print("==============================================================================")
 print(tvm.lower(s, [A, B, C], simple_mode=True))
+
 # indexing axes negatively to tile over two innermost axes
 xo, yo, xi, yi = s[C].tile(C.op.axis[-2], C.op.axis[-1],x_factor=2,y_factor=2)
 print("Larger schedule to apply tensorization of the Generic Schedule (after tiling):")
 print("==============================================================================")
 print(tvm.lower(s, [A, B, C], simple_mode=True))
-# Tensorize!
-print(s[C].op.axis[-1])
+
 # Get extent of most innermost original axis to act as a stride parameter in the tensorized version
 stride = s[C].op.axis[-1].dom.extent
+
+# Tensorize!
 s[C].tensorize(xi, intrin_ews(rows,cols,data_type,stride=stride))
 print("After tiling and applying the tensorization:")
 print("============================================")
