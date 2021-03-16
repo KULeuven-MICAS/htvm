ca5a13a46f53af2b57e8dfe15ccdacf90bf96973
Josse Van Delm <josse.vandelm@gmail.com>
2021-03-16 17:26:00 +0100

    Properly implement injective strategy, needs testing

--- python/tvm/topi/sirius/injective.py
+++ python/tvm/topi/sirius/injective.py
@@ -1,96 +1,10 @@
-#!/usr/bin/env python3
 
-################################### METADATA ###################################
-
-# Contributors: Vincent Tableau Roche
-# Contacts: vincent.tableau@esat.kuleuven.be
-# Creation Date: 2021-03-04
-# Language: Python3
-
-################################### IMPORTS ####################################
-
-# Standard library 
-# Your imports from the standard library go here 
-
-
-# External imports 
-# Your imports from other packages go here 
-
-
-# Internal imports
 import tvm
 from tvm import te  # Used for schedule manipulations
-from ..utils import is_empty_shape # Used for schedule_injective
 
 import logging
 logger = logging.getLogger("strategy")
 
-################################### CLASSES ####################################
-
-# Your classes go here 
-
-################################## FUNCTIONS ###################################
-
-def schedule_injective_from_existing(sch, out):
-    """Schedule for injective op from existing schedule.
-
-    Parameters
-    ----------
-    sch: Schedule
-         The schedule to update.
-    out: Tensor
-         The tensor representing the injective op.
-
-    Returns
-    -------
-    sch: Schedule
-         The updated schedule.
-    """
-    if len(sch[out].op.axis) >= 4:
-        fused = sch[out].fuse(sch[out].op.axis[0], sch[out].op.axis[1], sch[out].op.axis[2])
-        sch[out].parallel(fused)
-    elif len(sch[out].op.axis) >= 3:
-        fused = sch[out].fuse(sch[out].op.axis[0], sch[out].op.axis[1])
-        sch[out].parallel(fused)
-    elif len(sch[out].op.axis) >= 2:
-        sch[out].parallel(sch[out].op.axis[0])
-    return sch
-
-
-def intrin_ews(ro,co,data_type,stride):
-    a = te.placeholder((ro,co), dtype=data_type, name="a")
-    b = te.placeholder((ro,co), dtype=data_type, name="b")
-    c = te.compute((ro,co), lambda i,j: a[i,j] + b[i,j], name="c")
-
-    # Define buffers
-    # Offset factor --> optimize for vectorized buffering
-    # Strides are set by the factors that appear near the i.inner and j.inner
-    # In this case i.inner corresponds to the columnn dimension of the tensor, so:
-    Ab = tvm.tir.decl_buffer(a.shape, a.dtype, name="A", offset_factor=1, strides=[stride,1])
-    Bb = tvm.tir.decl_buffer(b.shape, b.dtype, name="B", offset_factor=1, strides=[stride,1])
-    Cb = tvm.tir.decl_buffer(c.shape, c.dtype, name="C", offset_factor=1, strides=[stride,1])
-
-    def intrin_func(ins, outs):
-        # create IR builder
-        ib = tvm.tir.ir_builder.create()
-        aa, bb = ins
-        cc = outs[0]
-        ib.emit(
-            tvm.tir.call_extern(
-                "int32",
-                "soma_wrapped_ews",
-                aa.access_ptr("r"), # "r" Results in a "1" in the 5th access pointer field
-                bb.access_ptr("r"),
-                cc.access_ptr("w"), # "w" Results in a "2" in the 5th access pointer field
-                a.shape[0], # width
-                a.shape[1], # height
-                1,          # channels
-                8,          # precision
-            )
-        )
-        return ib.get()
-
-    return te.decl_tensor_intrin(c.op, intrin_func, binds={a: Ab, b: Bb, c: Cb})
 
 def intrin_ews_soma(width, data_type, stride_outermost, stride_innermost):
     # Make height and channels variable
@@ -103,18 +17,15 @@ def intrin_ews_soma(width, data_type, stride_outermost, stride_innermost):
 
     c = te.compute(tensor_size, lambda i, j, k: a[i, j, k] + b[i, j, k], name="c")
 
-    # Preview a generic schedule
-    print("Generic Schedule for Element-wise Sum Compute to be tensorized:")
-    print("===============================================================")
-    preview = te.create_schedule(c.op)
-    print(tvm.lower(preview, [a, b, c], simple_mode=True))
-
     # Define buffers
     # Offset factor --> optimize for vectorized buffering
     # Strides are set by the factors that appear near the indexing elements
-    Ab = tvm.tir.decl_buffer(a.shape, a.dtype, name="A", offset_factor=1, strides=[stride_outermost, stride_innermost,1])
-    Bb = tvm.tir.decl_buffer(b.shape, b.dtype, name="B", offset_factor=1, strides=[stride_outermost, stride_innermost,1])
-    Cb = tvm.tir.decl_buffer(c.shape, c.dtype, name="C", offset_factor=1, strides=[stride_outermost, stride_innermost,1])
+    Ab = tvm.tir.decl_buffer(a.shape, a.dtype, name="A", offset_factor=1,
+                             strides=[stride_outermost, stride_innermost, 1])
+    Bb = tvm.tir.decl_buffer(b.shape, b.dtype, name="B", offset_factor=1,
+                             strides=[stride_outermost, stride_innermost, 1])
+    Cb = tvm.tir.decl_buffer(c.shape, c.dtype, name="C", offset_factor=1,
+                             strides=[stride_outermost, stride_innermost, 1])
 
     def intrin_func(ins, outs):
         # create IR builder
@@ -153,47 +64,32 @@ def schedule_injective(outs):
     sch: Schedule
         The computation schedule for the op.
     """
+    # Set the size of the intrinsic
+    ELEMENT_WISE_SIZE = 16
+
+    # Test if the outs is a list of multiple values, else make it a list
     outs = [outs] if isinstance(outs, te.tensor.Tensor) else outs
+    # If you pass a list to create_schedule, it will schedule the operations consecutively
     s = te.create_schedule([x.op for x in outs])
-    x = outs[0]
-
-    element_wise_size = 2
-
-    if outs.dim == 2:  # Use the old method
-        xo, yo, xi, yi = s[x].tile(x.op.axis[-2],
-                                   x.op.axis[-1],
-                                   x_factor=element_wise_size,
-                                   y_factor=element_wise_size)
-        stride = s[x].op.axis[-1].dom.extent
-        s[x].tensorize(xi, intrin_ews(element_wise_size,
-                                      element_wise_size,
-                                      x.dtype,
-                                      stride=stride))
-        return s
-
-    else:  # Use the new method
-        yo, yi = s[x].split(x.op.axis[-3], factor=element_wise_size)
-        stride_innermost = s[x].op.axis[-1].dom.extent
-        stride_outermost = s[x].op.axis[-2].dom.extent * s[x].op.axis[-1].dom.extent
-        s[x].tensorize(yi, intrin_ews_soma(element_wise_size,
-                                           x.dtype,
-                                           stride_outermost=stride_outermost,
-                                           stride_innermost=stride_innermost))
-        return s
-    #if list(s[x].op.axis):
-    #    # do not vectorize for broadcast
-    #    (io, ii) = s[x].split(list(s[x].op.axis)[-1], 4)
-    #    s[x].vectorize(ii)
-    #te.schedule.AutoInlineInjective(s)
-
-    #if not is_empty_shape(x.shape):
-    #    schedule_injective_from_existing(s, x)
-    #return s
-
-##################################### MAIN #####################################
-
-if __name__ == "__main__":
-    # The code to run when this file is used as a script goes here
-    pass
-
-##################################### EOF ######################################
+    # Try to tensorize the operators in this schedule
+    for x in outs:
+        if len(x.shape) < 3:
+            # Not enough dimensions to unroll over; just use vanilla schedule:
+            logger.warning(f'Operation {x.op.name}: too few tensor dimensions to tensorize: shape = {x.shape}')
+            continue
+        else:
+            if x.op.name == "T_add":  # Use element_wise sum intrinsic here!
+                yo, yi = s[x].split(x.op.axis[-3], factor=ELEMENT_WISE_SIZE)
+                stride_innermost = s[x].op.axis[-1].dom.extent
+                stride_outermost = s[x].op.axis[-2].dom.extent * s[x].op.axis[-1].dom.extent
+                s[x].tensorize(yi, intrin_ews_soma(ELEMENT_WISE_SIZE,
+                                                   x.dtype,
+                                                   stride_outermost=stride_outermost,
+                                                   stride_innermost=stride_innermost))
+                continue
+            else:  # No intrinsic available :( ; just use vanilla schedule:
+                logger.warning(f'Cannot tensorize operation {x.op.name}: No supported intrinsic available')
+                continue
+    # TODO: Find out why we have to do this :D
+    te.schedule.AutoInlineInjective(s)
+    return s
