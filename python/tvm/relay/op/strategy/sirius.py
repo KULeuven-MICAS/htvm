8b44b59a5c79d9b7e6a86308d7eec9621bb0d9ca
Josse Van Delm <josse.vandelm@gmail.com>
2021-04-01 11:27:27 +0200

    Add sirius strategy

--- python/tvm/relay/op/strategy/sirius.py
+++ python/tvm/relay/op/strategy/sirius.py
@@ -30,28 +30,56 @@ from .. import op as _op
 
 ################################## FUNCTIONS ###################################
 
-# conv2d_NCHWc
-@conv2d_NCHWc_strategy.register("cpu")
-def conv2d_NCHWc_strategy_sirius(attrs, inputs, out_type, target):
-    """conv2d_NCHWc generic strategy
 
-    attrs:
-        relay.attrs.Conv2DAttrs(0x55def97c1008)
-    inputs:
-        [Tensor(shape=[1, 64, 7, 7, 8], op.name=placeholder), Tensor(shape=[64, 64, 3, 3, 8, 8], op.name=placeholder)]
-    out_type:
-        Tensor[(1, 64, 7, 7, 8), float32]
-    target:
-        sirius -keys=cpu -link-params=0
-    """
+@conv2d_strategy.register("cpu")
+def conv2d_strategy_sirius(attrs, inputs, out_type, target):
     strategy = _op.OpStrategy()
-    logger.warning("Using generic implementation for conv2d_NCHWc.generic")
-    strategy.add_implementation(
-        wrap_compute_conv2d(topi.nn.conv2d_NCHWc, True, True),
-        wrap_topi_schedule(topi.sirius.schedule_conv2d_nchw),
-        name="conv2d_NCHWc.generic",
-    )
-    return strategy
+    logger.warning("Using SIRIUS conv2d strategy")
+    data, kernel = inputs
+    dilation_h,dilation_w =  attrs.get_int_tuple("dilation")
+    stride_h, stride_w = attrs.get_int_tuple("strides")
+    padding = attrs.get_int_tuple("padding")
+    groups = attrs.groups
+    layout = attrs.data_layout
+    kernel_layout = attrs.kernel_layout
+    if padding != "SAME":
+        raise NotImplementedError("Padding other than SAME not supported")
+
+    if layout == "NCHW":
+        if kernel_layout == "OIHW":
+            strategy.add_implementation(
+                wrap_compute_conv2d(topi.nn.conv2d_nchw, True, True),
+                wrap_topi_schedule(topi.sirius.schedule_conv2d)
+            )
+        else:
+            raise NotImplementedError(f"Data Layout:{layout}: Kernel layout{kernel_layout} not supported")
+    else:
+        raise NotImplementedError(f"Data layout{layout} not supported")
+
+
+
+# conv2d_NCHWc
+#@conv2d_NCHWc_strategy.register("cpu")
+#def conv2d_NCHWc_strategy_sirius(attrs, inputs, out_type, target):
+#    """conv2d_NCHWc generic strategy
+#
+#    attrs:
+#        relay.attrs.Conv2DAttrs(0x55def97c1008)
+#    inputs:
+#        [Tensor(shape=[1, 64, 7, 7, 8], op.name=placeholder), Tensor(shape=[64, 64, 3, 3, 8, 8], op.name=placeholder)]
+#    out_type:
+#        Tensor[(1, 64, 7, 7, 8), float32]
+#    target:
+#        sirius -keys=cpu -link-params=0
+#    """
+#    strategy = _op.OpStrategy()
+#    logger.warning("Using generic implementation for conv2d_NCHWc.generic")
+#    strategy.add_implementation(
+#        wrap_compute_conv2d(topi.nn.conv2d_NCHWc, True, True),
+#        wrap_topi_schedule(topi.sirius.schedule_conv2d_nchw),
+#        name="conv2d_NCHWc.generic",
+#    )
+#    return strategy
 
 
 @schedule_injective.register(["cpu"], override=True)
