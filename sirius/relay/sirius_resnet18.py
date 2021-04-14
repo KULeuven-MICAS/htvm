b24671726b67f3dd3ef5f335312112fdeaf7267a
Josse Van Delm <josse.vandelm@gmail.com>
2021-04-14 15:41:55 +0200

    Add work on relay examples for conv2d and resnet18

--- sirius/relay/sirius_resnet18.py
+++ sirius/relay/sirius_resnet18.py
@@ -4,13 +4,16 @@ from tvm.contrib import utils
 import tvm
 
 resnet18_mod, resnet18_params = relay.testing.resnet.get_workload(num_layers=18)
+print(resnet18_mod)
 
 # build
 with tvm.transform.PassContext(opt_level=3, config={'tir.disable_vectorize':True}):
-    resnet18_lib = relay.build(resnet18_mod, "sirius", params=resnet18_params)
+    resnet18_lib = relay.build(resnet18_mod, "llvm", params=resnet18_params)
 
 # path lib
 file_name = "resnet.so"
+
+print(resnet18_mod)
 
 # Getting module
 module = resnet18_lib.get_lib()
