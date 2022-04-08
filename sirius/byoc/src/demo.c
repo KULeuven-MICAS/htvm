a201d4f86d6725ebb4bc1e98eea657124dbf601c
Josse Van Delm <josse.vandelm@gmail.com>
2022-04-08 14:41:45 +0200

    Create inputs header from python file

--- sirius/byoc/src/demo.c
+++ sirius/byoc/src/demo.c
@@ -2,6 +2,10 @@
 #include <stdint.h>
 #include <tvm_runtime.h>
 #include "tvmgen_default.h"
+// Includes definition for input a and B
+#include <inputs.h>
+// For fancy 3D print
+#include <soma_debug.h>
 
 #define WORKSPACE_SIZE (16 * 1024)
 
@@ -9,50 +13,27 @@ int abs(int v) {return v * ((v > 0) - (v < 0)); }
 
 int main(int argc, char** argv) {
 	printf("Starting Demo\n");
-
 	printf("Allocating memory\n");
+
 	tvm_workspace_t app_workspace;
 	static uint8_t g_aot_memory[WORKSPACE_SIZE];
 	StackMemoryManager_Init(&app_workspace, g_aot_memory, WORKSPACE_SIZE);
+  int8_t *output = malloc(a_size * sizeof(int8_t));
 
-	printf("Creating test inputs\n");
-	uint32_t w = 3;
-	uint32_t h = 3;
-	uint32_t c = 3;
-	int8_t *a = malloc(w * h * c * sizeof(int8_t));
-	int8_t *b = malloc(w * h * c * sizeof(int8_t));
-	int8_t *output = malloc(w * h * c * sizeof(int8_t));
-	for (int i = 0; i < w; i++) {
-		for (int j = 0; j < h; j++) {
-		    for (int k = 0; k < c; k++) {
-			// Getting the position in the row-major-ordered arrays.
-			unsigned int position = i * h * c + j * c + k;
-			// One value for A.
-			a[position] = i * j + k;
-			// One value for B.
-			b[position] = i + j - 2 * k;
-		    }
-		}
-	}
-	printf("Running inference\n");
-	struct tvmgen_default_outputs outputs = {
+	printf("Assigning test inputs\n");
+  struct tvmgen_default_outputs outputs = {
 		.output = output,
 	};
 	struct tvmgen_default_inputs inputs = {
 		.a = a,
 		.b = b,
 	};
+
+	printf("Running inference\n");
 	tvmgen_default_run(&inputs, &outputs);
-    for (int i = 0; i < w; i++) {
-		for (int j = 0; j < h; j++) {
-		    for (int k = 0; k < c; k++) {
-			// Getting the position in the row-major-ordered arrays.
-			unsigned int position = i * h * c + j * c + k;
-			printf("[%d,%d,%d]<%d>\n", i, j, k, output[position]);
-		    }
-		}
-	}
 	printf("Inference done\n");
+	debug_print_3D_tensor(0,output,"%3d",a_w, a_h, a_c);\
+  printf("Program done\n");
 	return 0;
 }
 
