a8dc95b45ca6465a7956b50cb78c81dc7faa6ff4
Josse Van Delm <josse.vandelm@esat.kuleuven.be>
2022-07-25 11:19:03 +0200

    Refactor utils.py and relay.py
    This commit will refactor the disable_fusion to fuse_layers.
    This is for better readability, furthermore it includes an explicit
    shift field for the blocks defined in relay.py

--- sirius/byoc/relay.py
+++ sirius/byoc/relay.py
@@ -8,7 +8,7 @@ from tvm.relay.backend import Executor, Runtime
 import numpy as np
 
 
-def create_int8_conv_bias_act(x, name, weights_shape, act=False):
+def create_int8_conv_bias_act(x, name, weights_shape, act=False, shift_bits=0):
     # define weights and bias variables
     weights_name = name + '.weights'
     bias_name = name + '.bias'
@@ -26,8 +26,11 @@ def create_int8_conv_bias_act(x, name, weights_shape, act=False):
     # define ops
     x = relay.qnn.op.conv2d(x, w, relay.const(0), relay.const(0), relay.const(1.0), relay.const(1.0), weights_shape[-2:], channels=conv_channels, padding=(1, 1))
     x = relay.op.nn.bias_add(x, b)
-    x = relay.qnn.op.requantize(x, relay.const(1.0), relay.const(0), relay.const(1.0), relay.const(0), axis=1, out_dtype='int8')
+    #x = relay.op.right_shift(x, relay.const(shift_bits)) 
+    #x = relay.op.cast(x, 'int8')
+    x = relay.qnn.op.requantize(x, relay.const(1.0), relay.const(0), relay.const(float(2**shift_bits)), relay.const(0), axis=1, out_dtype='int8')
     # the fourth param of requantize contains the power-of-two division factor. All other constants will be ignored by soma codegen
+
 
     if act:
         x = relay.op.clip(x, a_min=-128, a_max=127)
@@ -40,17 +43,17 @@ def create_model():
     x = relay.var("input", relay.TensorType(input_shape, 'int8'))
 
     weights_shape = (16, 3, 3, 3)
-    x, params1 = create_int8_conv_bias_act(x, 'conv1', weights_shape, act=True)
+    x, params1 = create_int8_conv_bias_act(x, 'conv1', weights_shape, True, 2)
 
-    weights_shape = (32, 16, 3, 3)
-    x, params2 = create_int8_conv_bias_act(x, 'conv2', weights_shape, act=True)
+    #weights_shape = (32, 16, 3, 3)
+    #x, params2 = create_int8_conv_bias_act(x, 'conv2', weights_shape, True, 4)
 
-    weights_shape = (16, 32, 3, 3)
-    x, params3 = create_int8_conv_bias_act(x, 'conv3', weights_shape, act=True)
+    #weights_shape = (16, 32, 3, 3)
+    #x, params3 = create_int8_conv_bias_act(x, 'conv3', weights_shape, True, 4)
 
     # combine all params
-    params1.update(params2)
-    params1.update(params3)
+    #params1.update(params2)
+    #params1.update(params3)
     params = params1
 
     # create an IR module from the relay expression
@@ -64,4 +67,4 @@ mod, params = create_model()
 
 # compile the model
 model = TVMCModel(mod, params)
-tvmc_compile_and_unpack(model, target="soma, c")
+tvmc_compile_and_unpack(model, target="c", fuse_layers=True)
--- sirius/byoc/utils.py
+++ sirius/byoc/utils.py
@@ -7,7 +7,7 @@ from tvm.relay.backend import Executor, Runtime
 
 
 
-def tvmc_wrapper(model, target="soma, c", disable_fusion=False):
+def tvmc_wrapper(model, target="soma, c", fuse_layers=True):
     ''' 
     Utility wrapper for TVMC that sets supported
     :param model: TVMC model that you wish to compile
@@ -18,7 +18,7 @@ def tvmc_wrapper(model, target="soma, c", disable_fusion=False):
     assert((target == "soma, c") or (target == "c"))
     # This has to be set by default to use the C runtime
     pass_context_configs = ['tir.disable_vectorize=1']
-    if(disable_fusion):
+    if(fuse_layers == False):
         pass_context_configs.append('relay.FuseOps.max_depth=1')
     compile_model(tvmc_model=model,
                   target=target,
@@ -34,7 +34,7 @@ def tvmc_wrapper(model, target="soma, c", disable_fusion=False):
     return
             
 def tvmc_compile_and_unpack(model, target="soma, c", 
-        disable_fusion=False, build_path="./build"):
+        fuse_layers=True, build_path="./build"):
     # check if build folder exists
     path = pathlib.Path(build_path)
     if(path.is_dir()):
@@ -43,7 +43,7 @@ def tvmc_compile_and_unpack(model, target="soma, c",
         # make the build folder again
         path.mkdir()
     # Compile new model
-    tvmc_wrapper(model, target, disable_fusion)
+    tvmc_wrapper(model, target, fuse_layers)
     mlf_path = pathlib.Path("./build/model.tar")
     # extract mlf file
     mlf = tarfile.TarFile(mlf_path)
