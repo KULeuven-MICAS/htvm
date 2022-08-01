d3fe90a6d5c72708972cf30392004fce7ac92006
Josse Van Delm <josse.vandelm@esat.kuleuven.be>
2022-08-01 14:18:14 +0200

    Remove print statements from demo.c
    This demo.c included for demo purposes, should be generated by utils.py

--- sirius/byoc/src/demo.c
+++ sirius/byoc/src/demo.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdint.h>
 #include <tvm_runtime.h>
@@ -6,37 +7,31 @@
 int abs(int v) {return v * ((v > 0) - (v < 0)); }
 
 int main(int argc, char** argv) {
-	//printf("Starting Demo\n");
-	//printf("Allocating memory\n");
+    tvm_workspace_t app_workspace;
+    static uint8_t g_aot_memory[TVMGEN_DEFAULT_WORKSPACE_SIZE];
+    StackMemoryManager_Init(&app_workspace, g_aot_memory, TVMGEN_DEFAULT_WORKSPACE_SIZE);
+    // Sizes automatically added by utils.create_demo_file
+    	uint32_t input_size = 3072;
+	uint32_t output_size = 16384;
 
-	tvm_workspace_t app_workspace;
-	static uint8_t g_aot_memory[TVMGEN_DEFAULT_WORKSPACE_SIZE];
-	StackMemoryManager_Init(&app_workspace, g_aot_memory, TVMGEN_DEFAULT_WORKSPACE_SIZE);
-  uint32_t input_size = 1 * 3 * 32 * 32;
-  uint32_t output_size = 1 * 16 * 32 * 32;
-  int8_t *input = malloc(input_size * sizeof(int8_t));
-  int8_t *output = malloc(output_size * sizeof(int8_t));
-  for (uint32_t i = 0; i < input_size; i++){
-      input[i] = 1;
-  }
-
-
-	//printf("Assigning test inputs\n");
-  struct tvmgen_default_outputs outputs = {
-		.output = output,
-	};
-	struct tvmgen_default_inputs inputs = {
-		.input = input,
-	};
-
-	//printf("Running inference\n");
-	int32_t status = tvmgen_default_run(&inputs, &outputs);
-  free(input);
-  free(output);
-  if(status != 0){
-      abort();
-  }
-	//printf("Inference done\n");
-	return 0;
+    int8_t *input = malloc(input_size * sizeof(int8_t));
+    int8_t *output = malloc(output_size * sizeof(int8_t));
+    // Fill first input with ones
+    for (uint32_t i = 0; i < input_size; i++){
+        input[i] = 1;
+    }
+    struct tvmgen_default_outputs outputs = {
+    	.output = output,
+    };
+    struct tvmgen_default_inputs inputs = {
+    	.input = input,
+    };
+    int32_t status = tvmgen_default_run(&inputs, &outputs);
+    free(input);
+    free(output);
+    if(status != 0){
+        abort();
+    }
+    return 0;
 }
-
+    
