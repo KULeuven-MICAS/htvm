36d58fd0e70cb7f73ece4e97a1f16cd2c11d201f
Josse Van Delm <josse.vandelm@esat.kuleuven.be>
2022-09-15 17:34:30 +0200

    Refactor, merge plots, add more plots

--- sirius/byoc/plotting.py
+++ sirius/byoc/plotting.py
@@ -2,6 +2,7 @@ import numpy as np
 import csv
 
 import plotly.graph_objects as go
+from plotly.io import to_html
 
 import pandas as pd
 
@@ -43,49 +44,44 @@ def preprocess_cycle_counts(names, cycle_counts):
         else:
             total_tvm += cycles
     return total_dory, total_tvm, total_dory+total_tvm
-
-def bar_plot_total(names, cycle_counts, relative=True):
-    dory_list = []
-    tvm_list = []
-    opt_level = ["O0","O1","O2","O3"]
-    for cycles in cycle_counts:
-        dory, tvm,  tot = preprocess_cycle_counts(names, cycles)
-        if relative:
-            dory_list.append(dory/tot)
-            tvm_list.append(tvm/tot)
-        else:
-            dory_list.append(dory)
-            tvm_list.append(tvm)
-    if relative:
-        text_template = "%{y}"
-    else:
-        text_template = None
-    data = [
-            go.Bar(
-                   x=opt_level,
-                   y=dory_list,
-                   name="Dory",
-                   text=dory_list,
+    
+def bar_plot_total_wrapper( data_a, data_b,
+                            name_a, name_b,
+                            y_title="Cycles",
+                            x_categories=["O0","O1","O2","O3"], x_title="GCC Optimization Level",
+                            barmode="relative",
+                            fig_title=None, legend=True, legend_title=None,
+                            percentage=False):
+    text_template = "%{y}" if percentage else None
+    bar_a = go.Bar(
+               x=x_categories,
+               y=data_a,
+               name=name_a,
+               text=data_a,
+               texttemplate=text_template,
+              )
+    if data_b is not None:
+        bar_b = go.Bar(
+                   x=x_categories,
+                   y=data_b,
+                   name=name_b,
+                   text=data_b,
                    texttemplate=text_template
-                  ),
-             go.Bar(
-                    x=opt_level,
-                    y=tvm_list,
-                    name="TVM",
-                    text=tvm_list,
-                    texttemplate=text_template
-                    )
-             ]
+                  )
+        data = [bar_a, bar_b]
+    else:
+        data = [bar_a]
     fig = go.Figure(data=data)
-    y_title = "Cycles"
-    if relative:
+    if percentage:
         y_title = "Cycles (%)"
         fig.update_yaxes(tickformat=".2%", range=[0,1])
-    fig.update_layout(title="Relay Resnet20 Backend Comparison",
-                      barmode="relative", font_family="Droid Sans Mono",
-                      xaxis_title="GCC Optimization Level", yaxis_title=y_title,
-                      legend_title="Backend")
-    fig.write_html("bar_plot_total.html", auto_open=True)
+    fig.update_layout(title=fig_title,
+                  barmode=barmode, font_family="Droid Sans Mono",
+                  xaxis_title=x_title, yaxis_title=y_title,
+                  showlegend=legend,
+                  legend_title=legend_title, height=600)
+    return fig
+
 
 
 def bar_plot_individual(names, cycle_counts, log=True):
@@ -120,7 +116,7 @@ def bar_plot_individual(names, cycle_counts, log=True):
     if log:
         fig.update_xaxes(type="log")
         x_title = "Cycles (Log)"
-    fig.update_layout(title="Relay Resnet20 Cycle Rundown",
+    fig.update_layout(title="Relay Resnet20 Cycle Rundown - GCC Optimization",
                       barmode="group", font_family="Droid Sans Mono",
                       xaxis_title=x_title, yaxis_title="Kernel",
                       legend_title="GCC Optimization Level")
@@ -133,7 +129,7 @@ def bar_plot_individual(names, cycle_counts, log=True):
                         'ticktext': preprocess_names(names),
                         },
                      )
-    fig.write_html("bar_plot.html", auto_open=True)
+    return fig
 
 
 def make_names_unique(names):
@@ -141,13 +137,91 @@ def make_names_unique(names):
     # Stack the wrong data on top of each other
     names = [f"{i}_{name}" for i, name in enumerate(names)]
 
+def merge_figures(figures, path="plot.html"):
+    html_string = ""
+    # Only include JS library the first time
+    include_plotlyjs = True
+    for figure in figures:
+        html_string += to_html(figure, full_html=False, include_plotlyjs=include_plotlyjs)
+        include_plotlyjs = False
+    with open(path, "w") as plot:
+        plot.write(html_string)
+    
 
 if __name__ == "__main__":
-    names, cycle_counts_O0 = get_values("results/relay_resnet20_dory_fused_O0_individual.csv")
-    names, cycle_counts_O1 = get_values("results/relay_resnet20_dory_fused_O1_individual.csv")
-    names, cycle_counts_O2 = get_values("results/relay_resnet20_dory_fused_O2_individual.csv")
-    names, cycle_counts_O3 = get_values("results/relay_resnet20_dory_fused_O3_individual.csv")
-    cycle_counts = [cycle_counts_O0, cycle_counts_O1, cycle_counts_O2, cycle_counts_O3]
-    bar_plot_total(names, cycle_counts, relative=False)
-    bar_plot_individual(names, cycle_counts, log=True)
+    names = []
+    cycle_counts_individual = []
+    cycle_counts_global = []
+    dory_list = []
+    dory_rel_list = []
+    tvm_list = []
+    tvm_rel_list = []
+    tot_list = []
+    template = "results/relay_resnet20_dory_fused_{}_{}.csv"
+    for opt_level in ["O0","O1","O2","O3"]:
+        # Get individual counts
+        names, cycle_counts = get_values(template.format(opt_level,"individual"))
+        names = names
+        cycle_counts_individual.append(cycle_counts)
+        # Get global counts
+        dory, tvm, tot = preprocess_cycle_counts(names, cycle_counts)
+        tot_list.append(tot)
+        dory_rel_list.append(dory/tot)
+        tvm_rel_list.append(tvm/tot)
+        dory_list.append(dory)
+        tvm_list.append(tvm)
+        _names, cycle_counts = get_values(template.format(opt_level,"global"))
+        cycle_counts_global.append(cycle_counts[0])
+    figures = []
+    figures.append(bar_plot_individual(names, cycle_counts_individual, log=True))
+    figures.append(bar_plot_total_wrapper(data_a=dory_list, data_b=tvm_list,
+                                  name_a="Dory", name_b="TVM",
+                                  y_title="Cycles",
+                                  x_categories=["O0","O1","O2","O3"], x_title="GCC Optimization Level",
+                                  barmode="relative",
+                                  fig_title="TVM vs Dory - Backend comparison (Absolute Cycles) - GCC Optimization",
+                                  legend_title="Backend",
+                                  percentage=False))
+    figures.append(bar_plot_total_wrapper(data_a=dory_rel_list, data_b=tvm_rel_list,
+                                  name_a="Dory", name_b="TVM",
+                                  y_title="Cycles (%)",
+                                  x_categories=["O0","O1","O2","O3"], x_title="GCC Optimization Level",
+                                  barmode="relative",
+                                  fig_title="TVM vs Dory - Backend comparison (Relative Cycles) - GCC Optimization",
+                                  legend_title="Backend",
+                                  percentage=True))
+    figures.append(bar_plot_total_wrapper(data_a=cycle_counts_global, 
+                                  data_b=tot_list,
+                                  name_a="Globally Measured", name_b="Total of individual Kernels",
+                                  y_title="Cycles",
+                                  x_categories=["O0","O1","O2","O3"], x_title="GCC Optimization Level",
+                                  barmode="group",
+                                  fig_title="Runtime Overhead Measurement",
+                                  legend_title="Measurement",
+                                  percentage=False))
+    glob_fig = bar_plot_total_wrapper(data_a=cycle_counts_global, 
+                                  data_b=None,
+                                  name_a="Globally Measured", name_b=None,
+                                  y_title="Cycles",
+                                  x_categories=["O0","O1","O2","O3"], x_title="GCC Optimization Level",
+                                  barmode="group",
+                                  fig_title="Global Performance Improvement Chart - GCC Optimization",
+                                  legend_title="Measurement", legend=False,
+                                  percentage=False)
+    values = cycle_counts_global
+    labels = ["O0","O1","O2","O3"]
+    diffs = [max(values) - v for v in values]
+    speedup = [max(values)/v for v in values]
+    diff_labels = dict(zip(labels, diffs))
+    i = 0
+    for key, value in diff_labels.items():
+        if value != 0:
+            glob_fig.add_annotation(x=key, y=max(values)-value, ax=0, yanchor='bottom',
+                                ay=max(values), ayref='y', showarrow=True,
+                                arrowsize=2, arrowhead=1, text=f"-{value} ({speedup[i]:.1f}x)")
+        i += 1
+    glob_fig.add_traces(go.Scatter(x=labels, y=[max(values)]*len(labels), mode = 'lines',
+                   line=dict(color='black', width=1))) 
+    figures.append(glob_fig)
+    merge_figures(figures)
     
