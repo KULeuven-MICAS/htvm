98b350dfe10a434e1787c50e9befb308b62d6d1d
Josse Van Delm <josse.vandelm@esat.kuleuven.be>
2022-11-01 15:17:58 +0100

    Refactor relay_simple.py

--- diana/byoc/relay_simple.py
+++ diana/byoc/relay_simple.py
@@ -1,48 +1,21 @@
-from utils import (
-        tvmc_compile_and_unpack,
-        relay_soma_conv2d,
-        create_demo_file,
-        parse_cli_options,
-        create_random_array
-        make,
-        gdb
-        )
+import utils
 from profiler import insert_profiler
 import tvm
 import tvm.relay as relay
-import tvm.relay.transform as transform
 from tvm.driver.tvmc.model import TVMCModel
-from tvm.driver.tvmc.compiler import compile_model
-from tvm.relay.backend import Executor, Runtime
 import numpy as np
-
-# for reproducability
-np.random.seed(0)
 
 def create_model(weight_bits):
     input_shape = (1, 3, 16, 16)
     x = relay.var("input", relay.TensorType(input_shape, 'int8'))
 
     weights_shape = (32, 3, 3, 3)
-    weights = create_random_array(weights_shape, f'int{weight_bits}')
-    bias = create_random_array(weights_shape[0], 'int32')
-    x, params1 = relay_soma_conv2d(x, 'conv1', weights, bias, padding=(1, 1), act=True, shift_bits=4)
-
-    #weights_shape = (16, 32, 3, 3)
-    #weights = create_random_array(weights_shape, f'int{weight_bits}')
-    #bias = create_random_array(weights_shape[0], 'int32')
-    #x, params2 = relay_soma_conv2d(x, 'conv2', weights, bias, padding=(1, 1), act=True, shift_bits=5)
-
-    #weights_shape = (32, 16, 3, 3)
-    #weights = create_random_array(weights_shape, f'int{weight_bits}')
-    #bias = create_random_array(weights_shape[0], 'int32')
-    #x, params3 = relay_soma_conv2d(x, 'conv3', weights, bias, padding=(1, 1), act=False, shift_bits=3)
-
-    ## combine all params in one dictionary
-    #params1.update(params2)
-    #params1.update(params3)
+    weights = utils.create_random_array(weights_shape, f'int{weight_bits}')
+    bias = utils.create_random_array(weights_shape[0], 'int32')
+    x, params1 = utils.relay_soma_conv2d(x, 'conv1', weights, bias, 
+                                         padding=(1, 1), act=False, 
+                                         shift_bits=4)
     params = params1
-
     # create an IR module from the relay expression
     mod = tvm.ir.IRModule()
     mod = mod.from_expr(x)
@@ -51,22 +24,21 @@ def create_model(weight_bits):
 
 
 if __name__ == "__main__":
-    (target, device, measurement, interactive, 
-     fusion, weight_bits, gcc_opt) = parse_cli_options()
+    # for reproducability
+    np.random.seed(0)
+    # Get options from cli
+    args, opt_string = utils.parse_cli_options()
     # create the model
-    mod, params = create_model(weight_bits)
+    mod, params = create_model(args.weight_bits)
     model = TVMCModel(mod, params)
     # compile the model
-    tvmc_compile_and_unpack(model, target=target, fuse_layers=fusion)
-    create_demo_file(mod)
-    gdb_script = "gdb_demo_x86.sh"
-    fusion_name = "fused" if fusion else "unfused"
-    target_name = "dory" if target == "soma_dory, c" else "c"
-    csv_name = f"relay_simple_{target_name}_{fusion_name}" + \
-               f"_O{gcc_opt}_{measurement}.csv"
-    insert_profiler(measurement=measurement,
-                    interactive=interactive,
-                    csv_file=csv_name)
-    if not interactive:
-        make(device)
-        gdb(device, "build/demo", "gdb_demo_x86.sh")
+    utils.tvmc_compile_and_unpack(model, target=args.target, 
+                                  fuse_layers=args.fusion)
+    indefinite = True if args.measurement == "power" else False
+    utils.create_demo_file(mod, indefinite=indefinite)
+    insert_profiler(measurement=args.measurement,
+                    interactive=args.interactive,
+                    csv_file="relay_simple_"+opt_string+".csv")
+    if not args.interactive:
+        utils.make(args.device)
+        utils.gdb(args.device)
