136f438d4d1245c1ffa3714b6ea3d9cfbbc8fc86
Josse Van Delm <josse.vandelm@esat.kuleuven.be>
2022-11-04 16:41:06 +0100

    Update compare_outputs.py script to print difference between outputs

--- diana/byoc/compare_outputs.py
+++ diana/byoc/compare_outputs.py
@@ -1,10 +1,12 @@
 import utils
-
+import numpy as np
 
 if __name__ == "__main__":
+    np.set_printoptions(threshold=np.inf)
     demo_result = utils.get_gdb_output("demo.txt")
     demo_x86_result = utils.get_gdb_output("demo_x86.txt")
     if(np.ma.allequal(demo_result,demo_x86_result)):
         print("SUCCESS: x86 and Diana values are the same")
     else:
         print("FAIL: x86 and Diana values are NOT the same")
+        print(demo_result - demo_x86_result)
