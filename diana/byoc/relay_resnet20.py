e75ac511a30adde8721960816b6b865bd7a97e4d
Josse Van Delm <josse.vandelm@esat.kuleuven.be>
2022-11-04 19:55:19 +0100

    Refactor resnet20 test

--- diana/byoc/relay_resnet20.py
+++ diana/byoc/relay_resnet20.py
@@ -1,9 +1,4 @@
-from utils import (
-                   tvmc_compile_and_unpack, 
-                   relay_soma_conv2d, 
-                   create_demo_file,
-                   parse_cli_options
-                  )
+import utils
 from profiler import insert_profiler
 import tvm
 import tvm.relay as relay
@@ -18,36 +13,44 @@ def relay_res_block(input_tensor, name, input_channels, output_channels,
     # initial setup
     params = {}
     # 3x3 convolution block 1
-    weights_shape = (output_channels,input_channels,3,3)
-    x, params_out = relay_soma_conv2d(input_tensor, 
+    weights_shape = (output_channels, input_channels, 3, 3)
+    weights = utils.create_random_array(weights_shape, dtype="int8")
+    bias = utils.create_random_array(output_channels, dtype="int32")
+    x, params_out = utils.relay_soma_conv2d(input_tensor, 
                         name + '_3x3conv_0',
-                        weights_shape, np.ones(weights_shape, dtype=np.int8),
-                        np.ones(output_channels, dtype=np.int32),
-                        strides=(1,1))
+                        weights,
+                        bias,
+                        strides=(1,1),
+                        padding=(1,1))
     params.update(params_out)
     # 3x3 convolution block 2
-    weights_shape = (output_channels,output_channels,3,3)
-    x, params_out = relay_soma_conv2d(x, 
+    weights_shape = (output_channels, output_channels, 3, 3)
+    weights = utils.create_random_array(weights_shape, dtype="int8")
+    bias = utils.create_random_array(output_channels, dtype="int32")
+    x, params_out = utils.relay_soma_conv2d(x, 
                         name + '_3x3conv_1',
-                        weights_shape, np.ones(weights_shape, dtype=np.int8),
-                        np.ones(output_channels, dtype=np.int32),
-                        strides=((2,2) if strided else (1,1)))
+                        weights, 
+                        bias,
+                        strides=((2,2) if strided else (1,1)),
+                        padding=(1,1))
     params.update(params_out)
     # 1x1 skip layer convolution
     weights_shape = (output_channels,input_channels,1,1)
-    y, params_out = relay_soma_conv2d(input_tensor, 
+    weights = utils.create_random_array(weights_shape, dtype="int8")
+    bias = utils.create_random_array(output_channels, dtype="int32")
+    y, params_out = utils.relay_soma_conv2d(input_tensor, 
                         name + '_1x1conv',
-                        weights_shape, np.ones(weights_shape, dtype=np.int8),
-                        np.ones(output_channels, dtype=np.int32),
+                        weights,
+                        bias,
                         strides=((2,2) if strided else (1,1)))
     params.update(params_out)
-    #x = relay_soma_add(x, y)
     x = relay.add(x, y)
     return x, params
 
 def relay_res_layer(input_tensor, name, input_channels, output_channels, strided):
     params = {}
-    x, params_out = relay_res_block(input_tensor, name + "block_1", input_channels,
+    x, params_out = relay_res_block(input_tensor, name + "block_1",
+                                    input_channels,
                                     output_channels, False) 
     params.update(params_out)
     x, params_out = relay_res_block(x, name + "block_2", output_channels,
@@ -66,10 +69,10 @@ def create_model():
     input_tensor = relay.var("input", relay.TensorType(input_shape, 'int8'))
 
     # Connect resnet blocks
-    x, params_out = relay_soma_conv2d(input_tensor,
-                "conv1",
-                (16,3,3,3), np.ones((16,3,3,3), dtype=np.int8),
-                np.ones(16, dtype=np.int32))
+    weights = utils.create_random_array((16,3,3,3), dtype="int8")
+    bias = utils.create_random_array(16, dtype="int32")
+    x, params_out = utils.relay_soma_conv2d(input_tensor, "conv1", weights,
+                                      bias, padding=(1, 1))
     params.update(params_out)
     x, params_out = relay_res_layer(x, "res_layer_16", 16, 16, True)
     params.update(params_out)
@@ -85,8 +88,8 @@ def create_model():
     fc_weights_shape = (10,64)
     fc_weights = relay.var(fc_weights_name, 
                            relay.TensorType(fc_weights_shape, "int8"))
-    params.update({fc_weights_name: tvm.nd.array(np.ones(fc_weights_shape, 
-                                                          dtype=np.int8))})
+    fc_weights_values = utils.create_random_array(fc_weights_shape, "int8")
+    params.update({fc_weights_name: fc_weights_values})
     x = relay.nn.dense(x, fc_weights, out_dtype="int8")
     mod = tvm.ir.IRModule()
     mod = mod.from_expr(x)
@@ -95,18 +98,21 @@ def create_model():
 
 
 if __name__ == "__main__":
-    (target, device, measurement, interactive, 
-     fusion, weight_bits, gcc_opt) = parse_cli_options()
+    # for reproducability
+    np.random.seed(0)
+    # Get options from cli
+    args, opt_string = utils.parse_cli_options()
+    # create the model
     mod, params = create_model()
     model = TVMCModel(mod, params)
     # compile the model
-    tvmc_compile_and_unpack(model, target=target, fuse_layers=fusion)
-    create_demo_file(mod)
-    fusion_name = "fused" if fusion else "unfused"
-    target_name = "dory" if target == "soma_dory, c" else "c"
-    csv_name = f"relay_resnet20_{target_name}_{fusion_name}" + \
-               f"_O{gcc_opt}_{measurement}.csv"
-    insert_profiler(measurement=measurement,
-                    interactive=interactive,
-                    csv_file=csv_name)
-    make(device)
+    utils.tvmc_compile_and_unpack(model, target=args.target, 
+                                  fuse_layers=args.fusion)
+    indefinite = True if args.measurement == "power" else False
+    utils.create_demo_file(mod, indefinite=indefinite)
+    insert_profiler(measurement=args.measurement,
+                    interactive=args.interactive,
+                    csv_file="relay_resnet20_"+opt_string+".csv")
+    if not args.interactive:
+        utils.make(args.device)
+        utils.gdb(args.device)
