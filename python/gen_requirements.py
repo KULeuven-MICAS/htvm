bef2c60e6f20e3751d0a1bd9c2c7382399f421d5
Maarten Vandersteegen <maartenvandersteegen@gmail.com>
2023-08-04 15:11:14 +0200

    Undo of adding missing networkx dependency to gen_requirements.py
    networkx was already part of diana/requirements.txt so this was not
    needed. In the future we can integrate our python dependencies into
    gen_requirements.txt instead of a custom requirements file.

--- python/gen_requirements.py
+++ python/gen_requirements.py
@@ -151,16 +151,6 @@ REQUIREMENTS_BY_PIECE: RequirementsByPieceType = [
         "importer-tflite",
         ("Requirements for the TFLite importer", ["tensorflow", "tensorflow-estimator", "tflite"]),
     ),
-    # SOMA DORY codegen requirements
-    (
-        "soma-dory",
-        (
-            "Requirements for the SOMA DORY codegen",
-            [
-                "networkx",
-            ],
-        ),
-    ),
     (
         "tvmc",
         (
@@ -262,7 +252,6 @@ CONSTRAINTS = [
     ("h5py", "==2.10.0"),
     ("image", None),
     ("matplotlib", None),
-    ("networkx", "==3.1"),
     ("numpy", None),
     ("onnx", None),
     ("onnxoptimizer", None),
